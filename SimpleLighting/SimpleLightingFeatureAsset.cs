c2d23f3f8d47b236ca173a6ccc95a4e7b83fae0a
Yokuyo <56297955+HigashiSan@users.noreply.github.com>
2022-03-14 01:11:47 +0800

    Add files via upload

---
+++ SimpleLighting/SimpleLightingFeatureAsset.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "SimpleLightingFeatureAsset", menuName = "Rendering/SimpleLightingFeatureAsset")]
+public class SimpleLightingFeatureAsset : RaytracingRenderFeatureAsset
+{
+  public override RaytracingRenderFeature CreateInstance()
+  {
+    return new SimpleLightingFeature(this);
+  }
+}
